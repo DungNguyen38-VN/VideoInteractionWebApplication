a716fa6bd0c3e39ec950c0c5e39d17c4e00c7982
Jammy Nguyen <vandung.nguyen@primetals.com>
2024-08-19 16:56:35 +0700

    add multi Language

--- VideoInteraction.Web/Controllers/HomeController.cs
+++ VideoInteraction.Web/Controllers/HomeController.cs
@@ -1,23 +1,38 @@
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Globalization;
 using VideoInteraction.Models;
+using VideoInteraction.Utility;
+using VideoInteraction.Web.LangServices;
 
 namespace VideoInteraction.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private LanguageService _localization;
+        public HomeController(ILogger<HomeController> logger, LanguageService localization)
         {
             _logger = logger;
+            _localization = localization;
         }
 
         public IActionResult Index()
         {
+            var currentCulture = Thread.CurrentThread.CurrentCulture.Name;
             return View();
         }
-
+        public IActionResult ChangeLanguage(string culture)
+        {
+            if (string.IsNullOrEmpty(culture))
+            {
+                culture = "en-US";
+            }
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions() { Expires = DateTimeOffset.UtcNow.AddYears(1) });
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
         public IActionResult Privacy()
         {
             return View();
@@ -28,5 +43,21 @@ namespace VideoInteraction.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        //public  IActionResult ChangeLanguage(string lang)
+        //{
+        //    if (!string.IsNullOrEmpty(lang)) {
+        //        Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(lang);
+        //        Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
+
+        //    }
+        //    else
+        //    {
+        //        Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en");
+        //        Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+        //        lang = "en";
+        //    }
+        //    Response.Cookies.Append("Language", lang);    
+        //    return Redirect(Request.GetTypedHeaders().Referer.ToString());
+        //}
     }
 }
--- VideoInteraction.Web/Program.cs
+++ VideoInteraction.Web/Program.cs
@@ -5,6 +5,11 @@ using VideoInteraction.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using VideoInteraction.Utility;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using System.Reflection;
+using Microsoft.Extensions.Options;
+using VideoInteraction.Web.LangServices;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +20,28 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddRazorPages();
 
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddMvc().AddViewLocalization().AddDataAnnotationsLocalization(options =>
+{
+    options.DataAnnotationLocalizerProvider = (type, factory) =>
+    {
+        var assemblyName = new AssemblyName(typeof(SharedResource).GetTypeInfo().Assembly.FullName);
+        return factory.Create("SharedResource", assemblyName.Name);
+    };
+});
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new List<CultureInfo> {
+        new CultureInfo("en-US"),
+        new CultureInfo("zh-CN")
+    };
+    options.DefaultRequestCulture = new RequestCulture(culture: "zh-CN", uiCulture: "zh-CN");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+
+    options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+});
 //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 // Configure Identity services
 //builder.Services.AddDefaultIdentity<IdentityUser>(options =>
@@ -27,6 +54,7 @@ builder.Services.ConfigureApplicationCookie(options => {
     options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
 });
 
+builder.Services.AddScoped<LanguageService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 var app = builder.Build();
@@ -37,12 +65,28 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+};
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
 app.UseRouting();
+
+//app.Use(async (context, next) =>
+//{
+//    string cookie = string.Empty;
+//    if(context.Request.Cookies.TryGetValue("Language",out cookie))
+//    {
+//        System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(cookie);
+//        System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(cookie);
+//    }
+//    else
+//    {
+//        System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en");
+//        System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+//    }
+//    await next.Invoke();
+//});
 
 app.UseAuthorization();
 app.UseAuthentication();
